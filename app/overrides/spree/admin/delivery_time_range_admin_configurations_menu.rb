4f3c6d8121a6b1b0df2b8ba8e03bd9756ce375d8
Agustin Anfuso <anfusoagustin89@gmail.com>
2013-12-23 01:45:40 -0300

    Locales to get by.

--- app/overrides/spree/admin/delivery_time_range_admin_configurations_menu.rb
+++ app/overrides/spree/admin/delivery_time_range_admin_configurations_menu.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(virtual_path: "spree/admin/shared/_configuration_menu",
                      name: "delivery_time_range_admin_configurations_menu",
                      insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
-                     text: "<%= configurations_sidebar_menu_item t('delivery_time_ranges'), admin_delivery_time_ranges_url %>",
+                     text: "<%= configurations_sidebar_menu_item t('spree.admin.menu.delivery_time_ranges'), admin_delivery_time_ranges_url %>",
                      disabled: false)
