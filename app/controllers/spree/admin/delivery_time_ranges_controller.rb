196d6fe473528b56915c82640c1405fda0d01218
Agustin Anfuso <anfusoagustin89@gmail.com>
2014-01-10 19:26:20 -0300

    Delivery ranges sorted by the start delivery time.

--- app/controllers/spree/admin/delivery_time_ranges_controller.rb
+++ app/controllers/spree/admin/delivery_time_ranges_controller.rb
@@ -1,4 +1,7 @@
 module Spree
   class Admin::DeliveryTimeRangesController < Admin::ResourceController
+    def index
+      @delivery_time_ranges = Spree::DeliveryTimeRange.order(:start_delivery_time)
+    end
   end
 end
