aab0b37659d4505a0678206b986432573a474ae8
Agustin Anfuso <anfusoagustin89@gmail.com>
2014-03-12 02:01:42 -0300

    Time range validation.

--- app/models/spree/delivery_time_range.rb
+++ app/models/spree/delivery_time_range.rb
@@ -1,6 +1,7 @@
 module Spree
   class DeliveryTimeRange < ActiveRecord::Base
     validates :name, :start_delivery_time, :end_delivery_time, presence: true
+    validate :time_range
 
     scope :visible, where(visible: true)
 
@@ -13,5 +14,13 @@ module Spree
     def delivery_time_description
       "#{ name } - #{ delivery_time }"
     end
+
+    private
+    def time_range
+      if start_delivery_time > end_delivery_time
+        errors.add(:start_delivery_time, :less_than, count: :end_delivery_time)
+      end
+    end
+
   end
 end
